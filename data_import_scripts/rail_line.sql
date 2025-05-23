f4f80a226c9bbeee75b3c9266452698558871eac
Kousalya Potti <kousalyapotti1@gmail.com>
2025-05-23 11:14:04 -0400

    added data import scripts

---
+++ data_import_scripts/rail_line.sql
@@ -0,0 +1,9 @@
+INSERT INTO rail_line (rail_line_id,rail_line_name) VALUES
+	 (1,'WMATA Metro'),
+	 (2,'MARC: Brunswick Line'),
+	 (3,'Amtrak'),
+	 (4,'MARC: Penn Line'),
+	 (5,'VRE'),
+	 (6,'Metro'),
+	 (7,'MARC: Camden Line'),
+	 (8,'Light Rail');
